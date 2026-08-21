dd658ccc5ec8696ccec95f0e638cac0d302a1629
SebasDV201 <170909718+SebasDV201@users.noreply.github.com>
2026-08-20 19:45:34 -0500

    Corrige PronosticoFueraDeTiempoException a excepcion verificada

--- app/src/main/java/com/example/pronosticosmundial2026/excepciones/PronosticoFueraDeTiempoException.java
+++ app/src/main/java/com/example/pronosticosmundial2026/excepciones/PronosticoFueraDeTiempoException.java
@@ -1,6 +1,6 @@
 package com.example.pronosticosmundial2026.excepciones;
 
-public class PronosticoFueraDeTiempoException extends RuntimeException {
+public class PronosticoFueraDeTiempoException extends Exception {
     public PronosticoFueraDeTiempoException(String message) {
         super(message);
     }
